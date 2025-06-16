e23cafd2438f122ad2d8e768a3809ff41ba38e90
Sahil <Sahil@LAPTOP-JOSM6LUD>
2025-06-17 04:18:56 +0530

    Test added

---
+++ Java_Basics/src/ex_02_Literals/Lab035_Non_Primitive_Literal.java
@@ -0,0 +1,23 @@
+package ex_02_Literals;
+
+public class Lab035_Non_Primitive_Literal {
+
+	public static void main(String[] args) {
+		
+		//primitive Data Type - Defined by java 
+		// Also Defined by size Max, Min, Size Defined
+		
+	byte b = 10; // 1 Byte -> 8 Bits
+	int age = 65; // 4 Byte -> 32 Bits
+	
+	//Non primitive (Defined by users), Reference Data Type
+	// No Size, Max, Min
+	// Byte - 8, Bits - 64
+	String name = "Sahil"; //String is bunch of char.
+	int[] arrays_of_items = new int[10];
+	//String, Arrays, Classes, enum..
+	
+	String s = null;
+	//int age = null;
+	}
+}
